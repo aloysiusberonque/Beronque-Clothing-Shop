9c8122abfabf451a81344492d60aacdaf6a2ed15
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2022-12-08 23:03:20 +0800

    Implemented clothing shop

--- 11-clothing-shop/setup/src/components/App/App.tsx
+++ 11-clothing-shop/setup/src/components/App/App.tsx
@@ -1,23 +1,60 @@
-import { Link, Route, Routes } from 'react-router-dom';
-import { LinksWrapper, TitleWrapper, Wrapper } from './App.styled';
+import { Link, Route, Routes } from "react-router-dom";
+import { LinksWrapper, TitleWrapper, Wrapper } from "./App.styled";
 
-import { Cart } from '../Cart';
-import { Products } from '../Products';
+import { Cart } from "../Cart";
+import { Products } from "../Products";
+import { ShopContext } from "../context/shop";
+import { useReducer } from "react";
+import { addClothingItem, initialState, removeClothingItem, shopReducer, updateTotalCost } from "../reducer/shop";
+import { Product } from "../../models";
 
 export const App = () => {
+  const [state, dispatch] = useReducer(shopReducer, initialState);
+
+  const updatePrice = (items: [] = []) => {
+    let totalCost = 0;
+    items.forEach((clothingItem: { price: number; }) => (totalCost = totalCost + clothingItem.price));
+
+    dispatch(updateTotalCost(totalCost));
+  };
+
+  const removeItem = (clothingItem: Product) => {
+    const updatedCart = state.items.filter(
+      (currentProduct: Product) => currentProduct.name !== clothingItem.name
+    );
+    updatePrice(updatedCart);
+
+    dispatch(removeClothingItem(updatedCart));
+  };
+
+  const addToCart = (clothingItem: Product) => {
+    const updatedCart = state.items.concat(clothingItem);
+    updatePrice(updatedCart);
+
+    dispatch(addClothingItem(updatedCart));
+  };
+  
+  const value = {
+    totalCost: state.totalCost,
+    items: state.items,
+    addToCart,
+    removeItem
+  }
   return (
-    <Wrapper>
-      <TitleWrapper>
-        <h1>Clothing Shop Starter Project</h1>
-      </TitleWrapper>
-      <LinksWrapper>
-        <Link to='/'>Home</Link>
-        <Link to='/cart'>Cart</Link>
-      </LinksWrapper>
-      <Routes>
-        <Route path='/' element={<Products />} />
-        <Route path='/cart' element={<Cart />} />
-      </Routes>
-    </Wrapper>
+    <ShopContext.Provider value={value}>
+      <Wrapper>
+        <TitleWrapper>
+          <h1>Clothing Shop Starter Project</h1>
+        </TitleWrapper>
+        <LinksWrapper>
+          <Link to="/">Home</Link>
+          <Link to="/cart">Cart</Link>
+        </LinksWrapper>
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Wrapper>
+    </ShopContext.Provider>
   );
 };
--- 11-clothing-shop/setup/src/components/Cart/Cart.tsx
+++ 11-clothing-shop/setup/src/components/Cart/Cart.tsx
@@ -1,10 +1,19 @@
-import { ProductsWrapper, Title } from './Cart.styled';
+import { useContext } from "react";
+import { ShopContext, } from "../context/shop";
+import { Product } from "../../models";
+import { ProductCard } from "../ProductCard";
+import { ProductsWrapper, Title } from "./Cart.styled";
 
 export const Cart = () => {
+  const { items, totalCost } = useContext(ShopContext);
   return (
     <>
-      <Title>Your cart total is 00.00$</Title>
-      <ProductsWrapper></ProductsWrapper>
+      <Title>Your cart totalCost is {totalCost}.00$</Title>
+      <ProductsWrapper>
+        {items.map((clothingItem: Product, index) => (
+          <ProductCard {...clothingItem} key={index} />
+        ))}
+      </ProductsWrapper>
     </>
   );
 };
